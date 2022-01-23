0a5c0764ef70a1053e52efe4a76965bea3967858
Nguyễn Đạt <89953941+BeeAZZ@users.noreply.github.com>
2022-01-23 22:27:45 +0700

    Add files via upload

--- src/BeeAZZ/CustomItemCMD/Main.php
+++ src/BeeAZZ/CustomItemCMD/Main.php
@@ -20,22 +20,27 @@ Class Main extends PluginBase implements Listener{
    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
        switch($cmd->getName()){
            case "setitemname":
-           $name = $sender->getName();
+           if($sender instanceof Player){         
            $text = implode(" ", $args);
            $item = $sender->getInventory()->getItemInHand();
            $item->setCustomname($text);
            $sender->getInventory()->setItemInHand($item);
            $sender->sendMessage("You have renamed the item to " . $text . ".");
            break;
-           return true;
+           }else{
+            $sender->sendMessage("Please Use Command In Game");
+           }
            case "setitemlore":
-           $name = $sender->getName();
+          if($sender instanceof Player){
            $lore = implode(" ", $args);
            $lore = explode("\\n",$lore);
            $item = $sender->getInventory()->getItemInHand();
            $item->setLore($lore);
            $sender->getInventory()->setItemInHand($item);
            $sender->sendMessage("You have successfully ordered lore item.");
+          }else{
+            $sender->sendMessage("Please Use Command In Game");
+          }
        }
        return true;
    }
